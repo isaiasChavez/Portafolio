6ae172e119c6f276ff50cbe86998d68a35174210
Isaías Chávez Martínez <isaias.ch4vez@gmail.com>
2022-08-09 01:30:43 -0500

    ...

--- components/Dashboard/Footer.tsx
+++ components/Dashboard/Footer.tsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import { faFacebook} from '@fortawesome/free-solid-svg-icons'
 
 const Footer = () => {
   return (
@@ -8,7 +7,7 @@ const Footer = () => {
         <h3 className='text-3xl mb-2'>
           <span className='text-secondary'>N</span>orella
         </h3>
-        <div className='flex mb-3 justify-around w-full my-8 mb-12'>
+        <div className='flex justify-around w-full my-8 mb-12'>
           <div className='w-8 h-8 mr-2 cursor-pointer '> <img src="/assets/icons/icons8-facebook-f.svg"/></div>
           <div className='w-8 h-8 mr-2 cursor-pointer '> <img src="/assets/icons/icons8-github.svg"/></div>
           <div className='w-8 h-8 mr-2 cursor-pointer '> <img src="/assets/icons/icons8-instagram.svg"/></div>
