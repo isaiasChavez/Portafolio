fda723f8a0116c3644ad4d4fdcfb1c55202ecbb1
Isaías Chávez Martínez <isaias.ch4vez@gmail.com>
2022-08-09 22:17:46 -0500

    .

--- components/Dashboard/About/About.tsx
+++ components/Dashboard/About/About.tsx
@@ -3,7 +3,7 @@ import styles from "./styles.module.css";
 import { faAddressCard } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Image from "next/image";
-import { Link } from "react-scroll";
+
 
 export default memo(function About() {
   return (
@@ -47,10 +47,10 @@ export default memo(function About() {
             </h3>
             <hr className="w-1/12 mb-5" />
             <h2 className=" text-xl md:text-2xl mb-10">
-              I am a web and <b className="text-secondary">React Native developer</b>  with experience in <b className="text-secondary">Frontend</b> and <b className="text-secondary">Backend</b>.
+              I am a web and <b className="text-secondary">React Native developer</b> with experience in <b className="text-secondary">Frontend</b> and <b className="text-secondary">Backend</b>.
             </h2>
             <p className="text-sm md:text-lg font-normal">
-            Hi, my name is <b> Isaías Chávez</b> and I'm a computer Engineer with experience as FullStack/React Native Developer. I'm 23 and currently, I'm working as a Software engineer. I have made many applications in my career, giving support or making it from scratch. I have experience working on teams from America and have conversational English. I have good soft skills and I can work very well with my teams. I love to learn and listen. I can be a very good tool for your company.
+            Hi, my name is <b> Isaías Chávez</b> and I&lsquo;m a computer Engineer with experience as FullStack/React Native Developer. I&lsquo;m 23 and currently, I&lsquo;m working as a Software engineer. I have made many applications in my career, giving support or making it from scratch. I have experience working on teams from America and have conversational English. I have good soft skills and I can work very well with my teams. I love to learn and listen. I can be a very good tool for your company.
             </p>
           </div>
           <div className="mt-8">
